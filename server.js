5ae17c144e6f71a0bc8764e80cb73dd28fb30d56
sawbencraw <131794012+sawbencraw@users.noreply.github.com>
2024-02-24 21:13:52 -0700

    Update server.js
    added Sawyer Crawley

--- server.js
+++ server.js
@@ -13,4 +13,6 @@ app.get('/leonardo', (req,res) => {res.send("Hello Leonardo Mosiah Janes de Souz
 
 app.get('/giacomo', (req,res) => {res.send("Hello Giacomo Draghi")});
 
+app.get('/giacomo', (req,res) => {res.send("Hello Sawyer Crawley")});
+
 app.listen(port, ()=>console.log("Listening"));
