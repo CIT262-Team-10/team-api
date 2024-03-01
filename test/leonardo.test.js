a5c4116e030ce867f204dc5052b5a0f41c18ea90
LeoMosiah <leonardomosiah@gmail.com>
2024-03-01 14:47:22 +0100

    fix typo on name

--- test/leonardo.test.js
+++ test/leonardo.test.js
@@ -4,6 +4,6 @@ describe('Test GET request to /leonardo route using node-fetch', () => {
     it('responds with "Leonardo"', async () => {
         const response = await fetch('http://localhost:3000/leonardo');
         const name = await response.text();
-        expect(name.includes('Leonardo Mosiah Janes de Souza')).toBe(true); // Check if the response includes "Sawyer"
+        expect(name.includes('Leonardo Mosiah Janes de Souza')).toBe(true); // Check if the response includes "Leonardo"
     });
 });
