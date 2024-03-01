fd053aeda7ac63a006d762caa87dff369740094e
Isaac <icemaun@gmail.com>
2024-03-01 07:28:40 -0700

    isaac

--- test/Isaac.test.js
+++ test/Isaac.test.js
@@ -1,9 +1,9 @@
 const fetch = require('node-fetch2');
 
 describe('Test GET request to /Isaac route using node-fetch', () => {
-    it('responds with "Isaac"', async () => {
+    it('responds with ""', async () => {
         const response = await fetch('http://localhost:3000/Isaac');
         const name = await response.text();
-        expect(name.includes('Isaac Maughan')).toBe(true); // Check if the response includes "Leonardo"
+        expect(name.includes('Isaac Maughan')).toBe(true); // Check if the response includes "Isaac"
     });
 });
