23061d1f7703fb48e0e6ad782340c7c503b2425a
Isaac <icemaun@gmail.com>
2024-03-01 07:27:35 -0700

    isaac

--- test/Isaac.test.js
+++ test/Isaac.test.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch2');
 
-describe('Test GET request to /leonardo route using node-fetch', () => {
+describe('Test GET request to /Isaac route using node-fetch', () => {
     it('responds with "Leonardo"', async () => {
         const response = await fetch('http://localhost:3000/Isaac');
         const name = await response.text();
